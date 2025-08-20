b347e9e814c6d5a2d21c7056d3c4ff20b89bf0e1
mardbrito <marcelo4web@gmail.com>
2025-08-20 19:07:28 -0400

    first commit - frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+function App() {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    async function fetchTotal() {
+      const res = await fetch("/api/expenses/total-spent");
+      const data = await res.json();
+      setTotal(data.total);
+    }
+    fetchTotal();
+  }, []);
+
+  return (
+    <Card className="w-[350px] mx-auto">
+      <CardHeader>
+        <CardTitle>Total spent</CardTitle>
+        <CardDescription>The total amount you've spent</CardDescription>
+      </CardHeader>
+      <CardContent>{total}</CardContent>
+    </Card>
+  );
+}
+
+export default App;
